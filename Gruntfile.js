8e941ad00f7f331227116ba662937f9fe06dca33
Nikita Sedyh <nikita.sedyx.94@gmail.com>
2015-10-13 13:47:26 +0300

    initial v.0.0.0

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    webfont: {
+      icons: {
+        src: 'icons/*.svg',
+        dest: 'dist',
+        destCss: 'dist',
+        options: {
+          font: 'kx-icon-font',
+          relativeFontPath: 'dist',
+          template: 'font_template.scss',
+          htmlDemo: false
+        }
+      }
+    }
+  })
+
+  grunt.loadNpmTasks('grunt-webfont');
+
+  grunt.registerTask('gen_font', ['webfont'])
+}
